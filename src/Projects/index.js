225782eed7ad068404090d6ff33aef3856e9bae9
RuneMyrskog <runemyrskog@gmail.com>
2023-05-20 11:54:58 -0400

    added sneakpeak to projects

--- src/Projects/index.js
+++ src/Projects/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import "./styles.css";
+
 import TreeJSPic from "../images/TreeJSPic.png";
 import pathFindPic from "../images/pathFindPic.png";
 import RejuvenateHomePic from "../images/RejuvenateHomePic.png";
 import spaceAdventurePic from "../images/spaceAdventurePic.jpg";
 import CryptoTrackPic from "../images/cyptotrack.png";
+import SneakPeakPic from "../images/sneakpeak.png";
+
 import SectionTitle from '../Components/SectionTitle';
+
+
 import { GithubLink } from '../Components/Media';
 
 export default class Projects extends React.Component {
@@ -16,22 +21,11 @@ export default class Projects extends React.Component {
                 <div id="projectsContainer" className="sectionContainer">
                     <SectionTitle title="Projects" />
                     <div id="projectListContainer">
-                        
-                        <Project flipx={false} title={"Rejuvenate"} image={RejuvenateHomePic} alt="Rejuvenate website" href="https://projectrejuvenate.herokuapp.com">
-                            <p className="projectDescription">
-                                Full stack social media website targeted towards health enthusiasts, implementing full REST api and all CRUD operations.
 
-                            </p>
-                            <br />
-                            <span className="projectTechnologies">MongoDB Express React NodeJS</span>
-                            <br />
-                            <GithubLink href="https://github.com/RuneMyrskog/Rejuvenate-Website" />
-                        </Project>
-
-                        <Project flipx={true} title={"CryptoTrack"} image={CryptoTrackPic} alt="CryptoTrack" href="https://crypto-track-hq.vercel.app">
+                        <Project flipx={false} title={"CryptoTrack"} image={CryptoTrackPic} alt="CryptoTrack" href="https://crypto-track-hq.vercel.app">
                             <p className="projectDescription">
                                 Fully responsive, aesthetic website that displays real-time market data for top 50 cryptocurrencies
-                                
+
 
                             </p>
                             <br />
@@ -39,23 +33,31 @@ export default class Projects extends React.Component {
                             <br />
                             <GithubLink href="https://github.com/RuneMyrskog/CryptoTracker" />
                         </Project>
-                        
 
-                        <Project flipx={false} title={"AStar Visualized"} image={pathFindPic} alt="astar visualization" href="https://astarvisualize.herokuapp.com">
+                        <Project flipx={true} title={"SneakPeak"} image={SneakPeakPic} alt="SneakPeak" href="https://ecommerce-c0ccd.web.app">
                             <p className="projectDescription">
-                                Implementation and visualiztion of the A-Star path finding 
-                                algorithm. Written as a python application and as a 
-                                static webpage.
+                                Shoe store focused on simplicity with with all the expected ecommerce functions, synced to a realtime database
                             </p>
-                            <br/> 
-                            <span className="projectTechnologies">Python Javascript</span>
                             <br />
-                            <GithubLink href="https://github.com/RuneMyrskog/PathFindingVisualizer---static-web-page-version"/>
+                            <span className="projectTechnologies">Angular Firebase Bootstrap</span>
+                            <br />
+                            <GithubLink href="https://github.com/RuneMyrskog/CryptoTracker" />
                         </Project>
 
+                        <Project flipx={false} title={"Rejuvenate"} image={RejuvenateHomePic} alt="Rejuvenate website" href="https://projectrejuvenate.herokuapp.com">
+                            <p className="projectDescription">
+                                Full stack social media website targeted towards health enthusiasts, implementing full REST api and all CRUD operations
+                            </p>
+                            <br />
+                            <span className="projectTechnologies">MongoDB Express React NodeJS</span>
+                            <br />
+                            <GithubLink href="https://github.com/RuneMyrskog/Rejuvenate-Website" />
+                        </Project>
+
+                        
                         <Project flipx={true} title={"TreeJS"} image={TreeJSPic} alt="TreeJS" href="https://treejslib.herokuapp.com">
                             <p className="projectDescription">
-                                Front end javascript library to render custom tree structures in the DOM.
+                                Front end javascript library to render custom tree structures in the DOM
                             </p>
                             <br />
                             <span className="projectTechnologies">Javascript HTML CSS</span>
@@ -71,6 +73,18 @@ export default class Projects extends React.Component {
                             <span className="projectTechnologies">Java</span>
                             <br/>
                             <GithubLink href="https://github.com/RuneMyrskog/SpaceAdventure-A-2D-Android-Game"/>
+                        </Project>
+
+                        <Project flipx={true} title={"AStar Visualized"} image={pathFindPic} alt="astar visualization" href="https://astar-visualizer.vercel.app/">
+                            <p className="projectDescription">
+                                Implementation and visualiztion of the A-Star path finding
+                                algorithm. Written as a python application and as a
+                                static webpage.
+                            </p>
+                            <br />
+                            <span className="projectTechnologies">Python Javascript</span>
+                            <br />
+                            <GithubLink href="https://github.com/RuneMyrskog/PathFindingVisualizer---static-web-page-version" />
                         </Project>
 
                     </div>
---
+++ src/images/index.js
@@ -0,0 +1,15 @@
+import TreeJSPic from "./TreeJSPic.png";
+import pathFindPic from "./pathFindPic.png";
+import RejuvenateHomePic from "./RejuvenateHomePic.png";
+import spaceAdventurePic from "./spaceAdventurePic.jpg";
+import CryptoTrackPic from "./cyptotrack.png";
+import sneakPeakPic from "./sneakpeak.png";
+
+export default images = {
+    TreeJSPic,
+    pathFindPic,
+    RejuvenateHomePic,
+    spaceAdventurePic,
+    CryptoTrackPic,
+    sneakPeakPic
+}
